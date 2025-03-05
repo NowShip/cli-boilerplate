83b60e39273d2ca94e0261ae823f74a248d7fe72
Ali Samadi <alirs.dev@gmail.com>
2025-03-04 16:45:49 -0800

    feat: subscription is ready

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -58,7 +58,7 @@ function DialogOverlay({
     <DialogPrimitive.Overlay
       data-slot="dialog-overlay"
       className={cn(
-        "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/80",
+        "no-scrollbar data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/80",
         "grid place-items-center overflow-y-auto py-12",
         className
       )}
